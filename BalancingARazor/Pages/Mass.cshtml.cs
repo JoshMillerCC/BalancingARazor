10db4e9ef07967ba8cf942df2f1ad8e37834a092
joshua miller <joshua.miller@student.centralia.edu>
2020-05-12 21:53:40 -0700

    blank line

---
+++ BalancingARazor/Pages/Mass.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BalancingARazor.Pages
+{
+    public class MassModel : PageModel
+    {
+        public string convMethod { get; set; }
+        public string convMass { get; set; }
+        double newMass = 0;
+        string end = "";
+
+        [BindProperty]
+        public int MassConvOption { get; set; }
+        [BindProperty]
+        public double Mass { get; set; }
+        public void OnGet()
+        {
+            convMethod = "Welcome to the mass converter.";
+        }
+
+        public void OnPost()
+        {
+            if(MassConvOption == 1)
+            {
+                //Pounds to Ounces: multiply the mass value by 16
+                newMass = Mass * 16;
+                end = " ounces";
+            }
+            if(MassConvOption == 2)
+            {
+                //Ounces to Pounds: divide the mass value by 16
+                newMass = Mass / 16;
+                end = " pounds";
+            }
+            if(MassConvOption == 3)
+            {
+                //Grams to Pounds: divide the mass value by 454
+                newMass = Mass / 454;
+                end = " pounds";
+
+            }
+            if(MassConvOption == 4)
+            {
+                //Milligrams to Pounds: divide the mass value by 453592
+                newMass = Mass / 453492;
+                end = " pounds";
+            }
+            newMass = Math.Round(newMass, 2);
+            convMass = "Result: " + newMass.ToString() + end;
+        }
+    }
+}
---
+++ BalancingARazor/Pages/Temperature.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BalancingARazor.Pages
+{
+    public class TemperatureModel : PageModel
+    {
+        public string convMethod { get; set; }
+        public string convTemp { get; set; }
+        double newTemp = 0;
+        string end = "";
+
+        [BindProperty]
+        public int TempConvOption { get; set; }
+        [BindProperty]
+        public double Temp { get; set; }
+
+        public void OnGet()
+        {
+            convMethod = "Welcome to the temperature converter.";
+        }
+
+        public void OnPost()
+        {  
+            if (TempConvOption == 1)
+            {
+                //F2C: (32°F − 32) × 5 / 9 = 0°C
+                newTemp = ((Temp - 32) * 5 / 9);
+                end = " degrees Celcius";
+            }
+            if(TempConvOption == 2)
+            {
+                //C2F: (32°C × 9 / 5) +32 = 89.6°F
+                newTemp = ((Temp * 9 / 5) + 32);
+                end = " degrees Fahrenheit";
+            }
+            newTemp = Math.Round(newTemp, 2);
+            convTemp = "Result: " + newTemp.ToString() + end;
+        }
+    }
+}
